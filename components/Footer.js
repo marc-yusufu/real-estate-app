31b10e1debee52747716a4b918ddc7a39deb1e57
Marc <marcyusufu3@gmail.com>
2025-09-01 22:25:38 +0200

    search filter

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,6 @@ const Footer = () =>{
     return(
         <Box textAlign="center" p="5" marginTop="30px" color="gray.400" borderTop="1px solid" borderColor="gray.300">
             2025 RealEstate, Inc
-
         </Box>
     )
 }
